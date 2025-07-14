e7c79a3b461dd3a1f2ce10227c870ceb5f0790d8
benjamincabezas <benjamin.nicolas.cabezas@gmail.com>
2025-07-14 01:18:48 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const ramos = [
+  // 1° semestre
   { id: "anatomia", nombre: "Anatomía General y del Desarrollo", prereqs: [], semestre: 1 },
   { id: "quimica", nombre: "Química", prereqs: [], semestre: 1 },
   { id: "psicologia", nombre: "Psicología", prereqs: [], semestre: 1 },
@@ -6,38 +7,58 @@ const ramos = [
   { id: "saludCultura", nombre: "Salud, Cultura y Sociedad Global", prereqs: [], semestre: 1 },
   { id: "investigacion", nombre: "Metodología de la Investigación", prereqs: [], semestre: 1 },
 
+  // 2° semestre
   { id: "bioquimica", nombre: "Bioquímica Celular", prereqs: ["quimica"], semestre: 2 },
   { id: "bioestadistica", nombre: "Bioestadística", prereqs: [], semestre: 2 },
   { id: "cuidadosI", nombre: "Cuidados de Enfermería I", prereqs: [], semestre: 2 },
   { id: "saludPublica", nombre: "Salud Pública", prereqs: [], semestre: 2 },
+  { id: "filosofia", nombre: "Filosofía: ¿Para Qué?", prereqs: [], semestre: 2 },
 
+  // 3° semestre
   { id: "microbiologia", nombre: "Microbiología e Infección Clínica", prereqs: ["bioquimica"], semestre: 3 },
   { id: "fisiologia", nombre: "Fisiología", prereqs: ["bioquimica"], semestre: 3 },
   { id: "cuidadosFam", nombre: "Cuidados de Enfermería de la Persona y Familia", prereqs: [], semestre: 3 },
   { id: "educacionSalud", nombre: "Educación para la Salud", prereqs: ["cuidadosI", "naturaleza"], semestre: 3 },
   { id: "efg3", nombre: "Electivo de Formación General", prereqs: [], semestre: 3 },
 
+  // 4° semestre
   { id: "farmacologia", nombre: "Farmacología Clínica", prereqs: ["fisiologia"], semestre: 4 },
   { id: "fisiopatologia", nombre: "Fisiopatología General y de Sistemas", prereqs: ["fisiologia"], semestre: 4 },
   { id: "cuidadosII", nombre: "Cuidados de Enfermería II", prereqs: ["naturaleza", "cuidadosI", "saludPublica", "saludCultura"], semestre: 4 },
+  { id: "informaticaI", nombre: "Informática en Salud I", prereqs: [], semestre: 4 },
+  { id: "gestionI", nombre: "Gestión y Liderazgo I", prereqs: [], semestre: 4 },
   { id: "efg4", nombre: "Electivo de Formación General", prereqs: [], semestre: 4 },
 
+  // 5° semestre
   { id: "adultoMayor", nombre: "Cuidados Adulto y Persona Mayor", prereqs: ["cuidadosII"], semestre: 5 },
+  { id: "dimension", nombre: "Dimensión Interpersonal del Cuidado de Enfermería", prereqs: [], semestre: 5 },
   { id: "efg5_1", nombre: "Electivo de Formación General", prereqs: [], semestre: 5 },
   { id: "efg5_2", nombre: "Electivo de Formación General", prereqs: [], semestre: 5 },
 
+  // 6° semestre
   { id: "infancia", nombre: "Cuidados en Infancia y Adolescencia", prereqs: ["cuidadosII"], semestre: 6 },
+  { id: "etica", nombre: "Fundamentos Éticos del Ejercicio Profesional", prereqs: [], semestre: 6 },
+  { id: "optativo6", nombre: "Optativo de Profundización", prereqs: [], semestre: 6 },
   { id: "efg6", nombre: "Electivo de Formación General", prereqs: [], semestre: 6 },
 
-  { id: "familiar", nombre: "Enfermería Familiar y Comunitaria", prereqs: ["cuidadosII"], semestre: 7 },
+  // 7° semestre
+  { id: "familiar", nombre: "Enfermería en Salud Familiar y Comunitaria", prereqs: ["cuidadosII"], semestre: 7 },
   { id: "mental", nombre: "Cuidados en Salud Mental", prereqs: ["cuidadosII"], semestre: 7 },
+  { id: "investigacion7", nombre: "Investigación", prereqs: [], semestre: 7 },
   { id: "efg7", nombre: "Electivo de Formación General", prereqs: [], semestre: 7 },
 
-  { id: "cronicos", nombre: "Cuidados Condiciones Crónicas", prereqs: ["adultoMayor", "infancia", "familiar", "mental"], semestre: 8 },
+  // 8° semestre
+  { id: "cronicos", nombre: "Cuidados en Personas con Condiciones Crónicas", prereqs: ["adultoMayor", "infancia", "familiar", "mental"], semestre: 8 },
+  { id: "informaticaII", nombre: "Informática en Salud II", prereqs: [], semestre: 8 },
+  { id: "gestionII", nombre: "Gestión y Liderazgo II", prereqs: [], semestre: 8 },
+  { id: "optativo8", nombre: "Optativo de Profundización", prereqs: [], semestre: 8 },
   { id: "efg8", nombre: "Electivo de Formación General", prereqs: [], semestre: 8 },
 
+  // 9° semestre
   { id: "internadoHosp", nombre: "Internado Hospitalario", prereqs: ["cronicos"], semestre: 9 },
-  { id: "internadoUrg", nombre: "Internado Urgencias", prereqs: ["cronicos"], semestre: 9 },
+  { id: "internadoUrg", nombre: "Internado en Urgencias", prereqs: ["cronicos"], semestre: 9 },
+
+  // 10° semestre
   { id: "internadoAmb", nombre: "Internado Ambulatorio", prereqs: ["internadoHosp", "internadoUrg"], semestre: 10 }
 ];
 
